030c095f6a1b218f68179a9315f904f3a3950e3b
Joscha Henningsen <joscha.henningsen@tum.de>
2022-04-06 12:56:18 +0200

    make map

--- commons.go
+++ commons.go
@@ -2,7 +2,7 @@ package commons
 
 // Unique filters duplicates of a slice l of elements that are reducible to a uint by the function toId, e.g. gorm models.
 func Unique[T any](l []T, toId func(T) uint) []T {
-	var unique map[uint]bool
+	unique := make(map[uint]bool)
 	var result []T
 	for _, e := range l {
 		id := toId(e)
